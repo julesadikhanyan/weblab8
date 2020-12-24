4f8cccd0555b4ab9b1bed7a8d7aab091adcc7835
Julia Adikhanyan <yuliya.adihanyan@yandex.ru>
2020-12-24 15:34:56 +0300

    commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('applicationButton').onclick = function () {
+    document.getElementById("nameInput").value = "";
+    document.getElementById("numberInput").value = "";
+}
+
